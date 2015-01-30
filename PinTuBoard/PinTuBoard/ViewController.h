db4f4543a341fcd5639e79dedc1f92871c48a6af
tangzhengyue <ty54d1@163.com>
2015-01-30 21:20:30 +0800

    初始化项目。

---
+++ PinTuBoard/PinTuBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PinTuBoard
+//
+//  Created by Walle on 15/1/30.
+//  Copyright (c) 2015年 Walle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PinTuBoard/PinTuBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PinTuBoard
+//
+//  Created by Walle on 15/1/30.
+//  Copyright (c) 2015年 Walle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
